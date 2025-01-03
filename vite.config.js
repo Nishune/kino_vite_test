ef3ca4fa4487e6eac530944fe93cf8a6e08d867a
Rikard Engström <rikardengstrom89@gmail.com>
2025-01-03 18:10:28 +0100

    woeifn

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => ({
   base: command === 'serve' ? '/' : '/kino_vite_test/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        kids: './kids.html',
+        about: './about.html',
+      },
+    },
+  },
   css: {
     devSourcemap: true,
   },
